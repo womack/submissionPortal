0580da5e81aee81b5a3c46f0bd01d4729beba8b4
womackx <womackx@live.com>
2017-11-19 16:09:22 +0000

    updated port..need a better way of swapping between dev and non dev environments

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ mongoose.connect("mongodb://localhost/submissionDB", {useMongoClient: true});
 
 //Express
 const app = express();
-const port = 3001;
+const port = 3000;
 app.use(cors({origin: "http://submission.ewomack.com"}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
